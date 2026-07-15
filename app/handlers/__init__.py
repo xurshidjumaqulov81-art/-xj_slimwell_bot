1e6b6abfd7502ad174b4065a503ab3468d642859
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-16 04:52:31 +0500

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,7 +1,14 @@
 from aiogram import Router
 
-from app.handlers.start import router as start_router
+from app.handlers.profile import (
+    router as profile_router,
+)
+from app.handlers.start import (
+    router as start_router,
+)
 
 
 router = Router()
+
 router.include_router(start_router)
+router.include_router(profile_router)
