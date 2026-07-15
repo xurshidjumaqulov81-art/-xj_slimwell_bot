1872500671a64c9da344e77f6f27c09000af01b7
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-16 04:40:07 +0500

    Create db.py

---
+++ app/database/db.py
@@ -0,0 +1,31 @@
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.config import get_settings
+from app.database.models import Base
+
+
+settings = get_settings()
+
+
+engine = create_async_engine(
+    settings.async_database_url,
+    pool_pre_ping=True,
+)
+
+
+SessionFactory = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+async def init_db() -> None:
+    async with engine.begin() as connection:
+        await connection.run_sync(
+            Base.metadata.create_all,
+        )
