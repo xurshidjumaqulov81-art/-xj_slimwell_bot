b4d523e8127eaf8976958c552fcced80a80571c4
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-16 04:41:56 +0500

    Create loader.py

---
+++ app/loader.py
@@ -0,0 +1,19 @@
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from app.config import get_settings
+
+
+settings = get_settings()
+
+
+bot = Bot(
+    token=settings.bot_token,
+    default=DefaultBotProperties(
+        parse_mode=ParseMode.HTML,
+    ),
+)
+
+
+dispatcher = Dispatcher()
