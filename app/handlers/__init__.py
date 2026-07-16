652a9f93a5a319eb417aa3a61ff31de65714cc1a
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-16 15:58:23 +0500

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,23 +1,12 @@
 from aiogram import Router
 
-from app.handlers.body import (
-    router as body_router,
-)
-from app.handlers.exercises import (
-    router as exercises_router,
-)
-from app.handlers.profile import (
-    router as profile_router,
-)
-from app.handlers.results import (
-    router as results_router,
-)
-from app.handlers.slimwell import (
-    router as slimwell_router,
-)
-from app.handlers.start import (
-    router as start_router,
-)
+from app.handlers.start import router as start_router
+from app.handlers.profile import router as profile_router
+from app.handlers.body import router as body_router
+from app.handlers.slimwell import router as slimwell_router
+from app.handlers.exercises import router as exercises_router
+from app.handlers.results import router as results_router
+from app.handlers.habits import router as habits_router
 
 
 router = Router()
@@ -28,3 +17,4 @@ router.include_router(body_router)
 router.include_router(slimwell_router)
 router.include_router(exercises_router)
 router.include_router(results_router)
+router.include_router(habits_router)
