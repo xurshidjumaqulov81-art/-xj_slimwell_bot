e69866596e3a24221b0a05974eda5bd895677f41
xurshidjumaqulov81-art <xurshidjumaqulov81@gmail.com>
2026-07-16 15:56:36 +0500

    Update states.py

--- app/states.py
+++ app/states.py
@@ -1,45 +1,36 @@
 from aiogram.fsm.state import State, StatesGroup
 
 
-class Onboarding(StatesGroup):
+class Registration(StatesGroup):
     language = State()
-
-    personal_id = State()
-
     name = State()
-
     age = State()
-
     gender = State()
-
     height = State()
-
     weight = State()
-
     goal = State()
-
     activity = State()
 
 
-class EditProfile(StatesGroup):
-    value = State()
-
-
-class FoodScan(StatesGroup):
-    photo = State()
-
-    confirm = State()
+class ProfileEdit(StatesGroup):
+    name = State()
+    age = State()
+    height = State()
+    weight = State()
 
 
 class HabitInput(StatesGroup):
     water = State()
-
     steps = State()
-
     sleep = State()
-
     weight = State()
 
 
-class AdminSearch(StatesGroup):
-    personal_id = State()
+class FoodInput(StatesGroup):
+    photo = State()
+    description = State()
+
+
+class AdminState(StatesGroup):
+    broadcast = State()
+    search = State()
